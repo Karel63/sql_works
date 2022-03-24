448dc0babd4c73e0f5930ad4189c305ba5f7e72b
Karel63 <76937128+Karel63@users.noreply.github.com>
2022-03-24 10:32:48 +0100

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,6 +1,11 @@
+/* vytvořit tabulku */
 CREATE TABLE demo (
+    /* atribut id pro entity */
     id int UNSIGNED NOT NULL AUTO_INCREMENT,
+    /* atribut jméno pro entity */
     jmeno varchar(50) NOT NULL,
+    /* atribut narozené pro entity */
     narozeni datetime NOT NULL,
+    /* atribut věk pro entity */
     vek tinyint NOT NULL UNSIGNED,
 );
