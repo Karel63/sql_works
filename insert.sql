7a8a9938d7b541843b1579532fd031639948a771
Karel63 <76937128+Karel63@users.noreply.github.com>
2022-03-24 10:39:27 +0100

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,15 @@
+/*
+vložení nového záznamu
+úpravu záznamu
+smazání záznamu
+výběr s projekcí
+výběr s projekcí a novým dopočítaným sloupcem
+výběr se selekcí (užijte porovnání i logické spojky)
+výběr s řazením
+výběr s limitem
+práci s textem
+práci s datumem 
+*/
+INSERT INTO demo
+UPDATE 
+SELECT 
