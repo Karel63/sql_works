0c6d88dce55f0d3672780e0a989a21c4a9d33990
Karel63 <76937128+Karel63@users.noreply.github.com>
2022-03-25 12:07:39 +0100

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,18 +1,18 @@
-/*
-úpravu záznamu
-smazání záznamu
-výběr s projekcí
-výběr s projekcí a novým dopočítaným sloupcem
-výběr se selekcí (užijte porovnání i logické spojky)
-výběr s řazením
-výběr s limitem
-práci s textem
-práci s datumem 
-*/
-
-/* vložení jména a věku */
-INSERT INTO demo (jmeno, vek) VALUES ("Karel", 16);
+/* vložení jména, věku a narození pro Karla */
+INSERT INTO demo (jmeno, vek, narozeni) VALUES ("Karel", 16, 2005-06-17);
+/* vložení jména, věku a narození pro Evžena */
+INSERT INTO demo (jmeno, vek, narozeni) VALUES ("Evžen", 19, 2001-01-05);
 /* updatování jména */
 UPDATE demo SET jmeno = "Jan" WHERE id = 1;
 /* Smazání záznamu s id 2 */
 DELETE FROM demo WHERE id = 2;
+/* Vybrání všeho z tabulky demo */
+SELECT * FROM demo
+/* Vybrání jména a věku z tabulky demo */
+SELECT jmeno, vek FROM demo
+/* Vybrání jména a věku kde id je rovno 1 nebo 2 */
+SELECT jmeno, vek FROM demo WHERE id = 1 OR 2
+/* Vybrání jména a věku z tabulky demo, a seřazené podle jmána */
+SELECT jmeno, vek FROM demo ORDER BY jmeno
+/* Vybrání jména a věku z tabulky demo, seřazeno podle jmána, a vypsané pouze první 3 entity */
+SELECT jmeno, vek FROM demo ORDER BY jmeno LIMIT 3
