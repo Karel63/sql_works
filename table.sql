8279b2883d8a090871d678eaf8d949ea5f4cdece
Karel63 <76937128+Karel63@users.noreply.github.com>
2022-03-25 12:06:14 +0100

    Update table.sql

--- table.sql
+++ table.sql
@@ -4,8 +4,8 @@ CREATE TABLE demo (
     id int UNSIGNED NOT NULL AUTO_INCREMENT,
     /* atribut jméno pro entity */
     jmeno varchar(50) NOT NULL,
-    /* atribut narozené pro entity */
-    narozeni date NOT NULL,
     /* atribut věk pro entity */
     vek tinyint NOT NULL UNSIGNED,
+    /* atribut narozené pro entity */
+    narozeni date NOT NULL,
 );
